f20fe713ca7d3b4dfd37361a0c80d55549a9ae1b
Daniil <notdedinside@icloud.com>
2024-04-20 15:48:00 +0300

    implemented sample web api

---
+++ Domain/Entities/Hotel.cs
@@ -0,0 +1,37 @@
+﻿namespace Domain.Entities;
+
+public class Hotel
+{
+    public int Id { get; private init; }
+    public string Name { get; private set; }
+    public string Address { get; private set; }
+    public DateTime OpenSince { get; private set; }
+
+    public Hotel( int id, string name, string address, DateTime openSince )
+    {
+        Id = id;
+        Name = name;
+        Address = address;
+        OpenSince = openSince;
+    }
+
+    public void SetName( string name )
+    {
+        if ( string.IsNullOrWhiteSpace( name ) )
+        {
+            throw new ArgumentException( $"'{nameof( name )}' cannot be null or whitespace.", nameof( name ) );
+        }
+
+        Name = name;
+    }
+
+    public void SetAddress( string address )
+    {
+        if ( string.IsNullOrWhiteSpace( address ) )
+        {
+            throw new ArgumentException( $"'{nameof( address )}' cannot be null or whitespace.", nameof( address ) );
+        }
+
+        Address = address;
+    }
+}
---
+++ HotelManagement/Controllers/HotelsController.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Entities;
+using HotelManagement.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Controllers;
+
+// Описание работы с web api https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio
+[ApiController]
+[Route( "hotels" )]
+// http-протокол https://developer.mozilla.org/ru/docs/Web/HTTP
+// методы http - https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+public class HotelsController : ControllerBase
+{
+    private static List<Hotel> _hotels;
+
+    // статический конструктор запускается один раз для класса
+    static HotelsController()
+    {
+        _hotels = new();
+    }
+
+    // Http-метод GET
+    // GET подразумевает что мы запрашиваем данные с сервера, не меняем состояние на нем
+    // может содержать query-параметре в качестве метода фильтра/уточнения запроса данных
+    [HttpGet( "" )]
+    public IActionResult GetHotels()
+    {
+        return Ok( _hotels );
+    }
+
+    // Http-метод POST
+    // POST метод подразумевает изменение состояния на сервере, например, создание нового отеля
+    // Также содержит body - тело запроса, в нем передаются данные
+    [HttpPost( "" )]
+    public IActionResult CreateHotel( /*Говорим что данные имеют формат CreateHotelRequest и лежат в теле http-запроса*/ [FromBody] CreateHotelRequest request )
+    {
+        int id = _hotels.Count + 1;
+        Hotel newHotel = new( id, request.Name, request.Address, request.OpenSince );
+
+        _hotels.Add( newHotel );
+
+        // возвращает http-ответ со статусом 200-ОК
+        return Ok();
+    }
+
+    // Http-метод PUT
+    // PUT означает изменение состояние на сервере для сущ-их данных
+    [HttpPut( "{id:int}" )]
+    public IActionResult ModifyHotel( [FromRoute] int id, [FromBody] ModifyHotelRequest request )
+    {
+        // находим отель который пользователь хочет изменить
+        Hotel hotel = _hotels.FirstOrDefault( h => h.Id == id );
+
+        // если отеля нет - говорим об этом пользователю
+        if ( hotel is null )
+        {
+            return NotFound();
+        }
+
+        hotel.SetName( request.Name );
+        hotel.SetAddress( request.Address );
+
+        return Ok();
+    }
+
+    [HttpDelete( "{id:int}" )]
+    public IActionResult DeleteHotel( [FromRoute] int id )
+    {
+        // todo: implement
+        throw new NotImplementedException();
+    }
+}
+
+
